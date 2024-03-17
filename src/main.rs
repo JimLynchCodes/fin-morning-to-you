e0a93bd2ea80c4ff7ebbab4802b8183e06aaf658
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2024-03-17 11:02:55 -0400

    veddy nice

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    open::that("https://www.coindesk.com/price/bitcoin/").unwrap();
+    open::that("https://www.coindesk.com/price/ethereum/").unwrap();
+    open::that("https://www.coindesk.com/price/solana/").unwrap();
+    open::that("http://www.maximum-pain.com/options/spy").unwrap();
+    open::that("http://www.maximum-pain.com/options/spx").unwrap();
+    open::that("http://www.maximum-pain.com/options/nvda").unwrap();
+    open::that("https://www.cnn.com/markets/fear-and-greed").unwrap();
+    open::that("https://swaggystocks.com/dashboard/options-max-pain/spy").unwrap();
+    open::that("https://discord.com/channels/708365137660215327/765540788162920480").unwrap();
+    open::that("https://www.cnbc.com/the-morning-report/").unwrap();
+}
