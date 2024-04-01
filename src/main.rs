b36305bc0e4b5e4ace20f1cfa2f7848d201ef0cb
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2024-04-01 18:59:30 -0400

    for loop style

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 fn main() {
-    open::that("https://www.coindesk.com/price/bitcoin/").unwrap();
-    open::that("https://www.coindesk.com/price/ethereum/").unwrap();
-    open::that("https://www.coindesk.com/price/solana/").unwrap();
-    open::that("http://www.maximum-pain.com/options/spy").unwrap();
-    open::that("http://www.maximum-pain.com/options/spx").unwrap();
-    open::that("http://www.maximum-pain.com/options/nvda").unwrap();
-    open::that("https://www.cnn.com/markets/fear-and-greed").unwrap();
-    open::that("https://swaggystocks.com/dashboard/options-max-pain/spy").unwrap();
-    open::that("https://discord.com/channels/708365137660215327/765540788162920480").unwrap();
-    open::that("https://www.cnbc.com/the-morning-report/").unwrap();
+
+    let sites = vec![
+        "https://www.cnbc.com/the-morning-report/",
+        "https://www.coindesk.com/price/ethereum/",
+        "https://discord.com/channels/708365137660215327/765540788162920480",
+        "https://swaggystocks.com/dashboard/options-max-pain/spy",
+        "https://www.cnn.com/markets/fear-and-greed",
+        "http://www.maximum-pain.com/options/nvda",
+        "http://www.maximum-pain.com/options/SPX",
+        "http://www.maximum-pain.com/options/MSFT",
+        "http://www.maximum-pain.com/options/TSLA",
+        "http://www.maximum-pain.com/options/QQQ",
+        "https://www.coindesk.com/price/bitcoin/",
+        "https://www.coindesk.com/price/ethereum/",
+        "https://www.coindesk.com/price/solana/",
+    ];
+
+    for site in sites.iter() {
+        open::that(site).unwrap();
+    }
+
 }
